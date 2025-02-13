c80141029679c599b475e09ef5aec6d6e6a4b3f5
Sasha <st087955@student.spbu.ru>
2025-02-13 17:09:42 +0300

    Сделано отображение информации о погоде на странице

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import "./styles.css";
 
 async function getWeatherInfo(location = 'london'){
 
+    const locat = document.querySelector('#location'),
+          temp = document.querySelector('#temperature'),
+          feelsLike  = document.querySelector('#feels-like'),
+          wind = document.querySelector('#wind-mph'),
+          humidity = document.querySelector('#humidity');
+
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${Date.now().toString().slice(0, 10)}?key=A58BLS2KXH4Q7BGEBG2KRWNK5`, {mode: 'cors'});
     const weatherData = await response.json();
-    console.log(weatherData.currentConditions.temp, 
-                weatherData.currentConditions.feelslike, 
-                weatherData.currentConditions.windspeed, 
-                weatherData.currentConditions.humidity);
+    
+    locat.textContent = weatherData.resolvedAddress; 
+    temp.textContent = weatherData.currentConditions.temp; 
+    feelsLike.textContent = `Feels like: ${weatherData.currentConditions.feelslike}`; 
+    wind.textContent = `Wind: ${weatherData.currentConditions.windspeed} MPH`;
+    humidity.textContent = `Humidity: ${weatherData.currentConditions.humidity} %`;
 }
 
-getWeatherInfo('Moscow');
+getWeatherInfo();
