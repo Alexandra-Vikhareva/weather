a4db5387292db4367947794a549fab6f915b4d9d
Sasha <st087955@student.spbu.ru>
2025-02-13 16:46:17 +0300

    Написана функция, получающая данные о погоде в данный момент в каком-либо городе

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+async function getWeatherInfo(location = 'london'){
+
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${Date.now().toString().slice(0, 10)}?key=A58BLS2KXH4Q7BGEBG2KRWNK5`, {mode: 'cors'});
+    const weatherData = await response.json();
+    console.log(weatherData.currentConditions.temp, 
+                weatherData.currentConditions.feelslike, 
+                weatherData.currentConditions.windspeed, 
+                weatherData.currentConditions.humidity);
+}
+
+getWeatherInfo('Moscow');
