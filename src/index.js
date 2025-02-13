e6a9996fbc94d79370e1fa1c436e319b40c851f3
Sasha <st087955@student.spbu.ru>
2025-02-13 22:23:14 +0300

    Добавлены отловы ошибок в асинхронных функциях

--- src/index.js
+++ src/index.js
@@ -1,36 +1,45 @@
 import "./styles.css";
 import { createClient } from 'pexels';
+import forestImage from "./forest.jpg";
+
 
 async function getPhoto(query = 'sky'){
-    const client = createClient('7u3DN54OPhwE6qLJxQSKWJyTp0lz2gMpz6yWbehbv0QumL3lhxOGunaT');
-    const adress = await client.photos.search({query, per_page: 1, orientation: 'landscape'});
-    const photo = await adress.photos[0].src['original'];
-    document.querySelector('#main-container').style.backgroundImage=`url(${photo})`;
+    try{
+        const client = createClient('7u3DN54OPhwE6qLJxQSKWJyTp0lz2gMpz6yWbehbv0QumL3lhxOGunaT');
+        const adress = await client.photos.search({query, per_page: 1, orientation: 'landscape'});
+        const photo = await adress.photos[0].src['original'];
+        document.querySelector('#main-container').style.backgroundImage=`url(${photo})`;
+    }catch{
+        document.querySelector('#main-container').style.backgroundImage=`url(${forestImage})`;
+    }
 }
 
 async function getWeatherInfo(location = 'london'){
+    try{
+        const locat = document.querySelector('#location'),
+            temp = document.querySelector('#temperature'),
+            feelsLike  = document.querySelector('#feels-like'),
+            wind = document.querySelector('#wind-mph'),
+            humidity = document.querySelector('#humidity');
 
-    const locat = document.querySelector('#location'),
-          temp = document.querySelector('#temperature'),
-          feelsLike  = document.querySelector('#feels-like'),
-          wind = document.querySelector('#wind-mph'),
-          humidity = document.querySelector('#humidity');
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${Date.now().toString().slice(0, 10)}?key=A58BLS2KXH4Q7BGEBG2KRWNK5`, {mode: 'cors'});
+        const weatherData = await response.json();
 
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${Date.now().toString().slice(0, 10)}?key=A58BLS2KXH4Q7BGEBG2KRWNK5`, {mode: 'cors'});
-    const weatherData = await response.json();
-
-    locat.textContent = weatherData.resolvedAddress; 
-    if (temp.classList.contains('celsius')){
-        temp.textContent = FtoC(weatherData.currentConditions.temp); 
-        feelsLike.textContent = `Feels like: ${FtoC(weatherData.currentConditions.feelslike)}`; 
-    }else{
-        temp.textContent = Math.round(weatherData.currentConditions.temp); 
-        feelsLike.textContent = `Feels like: ${Math.round(weatherData.currentConditions.feelslike)}`; 
+        locat.textContent = weatherData.resolvedAddress; 
+        if (temp.classList.contains('celsius')){
+            temp.textContent = FtoC(weatherData.currentConditions.temp); 
+            feelsLike.textContent = `Feels like: ${FtoC(weatherData.currentConditions.feelslike)}`; 
+        }else{
+            temp.textContent = Math.round(weatherData.currentConditions.temp); 
+            feelsLike.textContent = `Feels like: ${Math.round(weatherData.currentConditions.feelslike)}`; 
+        }
+        
+        wind.textContent = `Wind: ${weatherData.currentConditions.windspeed} MPH`;
+        humidity.textContent = `Humidity: ${weatherData.currentConditions.humidity} %`;
+        getPhoto(weatherData.description);
+    }catch{
+        window.alert('bad request');
     }
-    
-    wind.textContent = `Wind: ${weatherData.currentConditions.windspeed} MPH`;
-    humidity.textContent = `Humidity: ${weatherData.currentConditions.humidity} %`;
-    getPhoto(weatherData.description);
 }
 
 const search = document.querySelector('#search');
